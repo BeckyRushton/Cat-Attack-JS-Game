eec969f1a88c646d8779068031cce53539f96f6a
Becky Rushton <beckyrushton30@gmail.com>
2023-01-31 16:38:35 +0000

    Designed mobile layout

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import catDeck from "./deck.js";
+const reset = document.querySelector("#resetGame");
 const computerCard = document.querySelector(".catCardComputer__card");
 const userCard = document.querySelector(".catCardUser__card");
 
@@ -20,8 +21,30 @@ const makeCard = (catCharacters) => {
 // Will also need to choose 2 from deck and not populate the computer
 // card until duel button is pressed
 const handleChooseCard = () => {
-  const randomCard = catDeck[(Math.random() * catDeck.length) | 0];
-  console.log(randomCard);
-  userCard.innerHTML = makeCard(randomCard);
+  const randomCardUser = catDeck[(Math.random() * catDeck.length) | 0];
+  console.log(randomCardUser);
+  const randomCardComputer = catDeck[(Math.random() * catDeck.length) | 0];
+  console.log(randomCardComputer);
+  userCard.innerHTML = makeCard(randomCardUser);
+  computerCard.innerHTML = makeCard(randomCardComputer);
 };
 userCard.addEventListener("click", handleChooseCard);
+
+// Function to compare the user's chosen attack method with computers stat
+// const handleDuel = (event) => {
+//   const attack = event.target.value;
+//   const userCat = randomCardUser;
+//   const computerCat = randomCardComputer;
+//   if (userCat > computerCard) {
+//     return "winner";
+//   } else if (userCat == computerCat) {
+//     return "draw";
+//   } else {
+//     return "lost";
+//   }
+// };
+
+// Function to reset game page
+// const handleReset = (event) => {
+//   const
+// };
